fbafe8d169a117affadf2bfc5ec2d7af637b1710
Alexander <Alexander@172.16.210.57>
2017-10-04 12:16:49 -0500

    Creación de entidades: Entregables, Fase y Nivel

--- src/main/java/edu/unmsm/sistemas/sgpy/entities/Fase.java
+++ src/main/java/edu/unmsm/sistemas/sgpy/entities/Fase.java
@@ -10,5 +10,44 @@ package edu.unmsm.sistemas.sgpy.entities;
  * @author lucho
  */
 public class Fase {
+    private int CodFase;
+    private String DesFase;
+    private String Vigencia;
+
+    public Fase(int CodFase, String DesFase, String Vigencia) {
+        this.CodFase = CodFase;
+        this.DesFase = DesFase;
+        this.Vigencia = Vigencia;
+    }
+    
+    
+    public int getCodFase() {
+        return CodFase;
+    }
+
+    public void setCodFase(int CodFase) {
+        this.CodFase = CodFase;
+    }
+
+    public String getDesFase() {
+        return DesFase;
+    }
+
+    public void setDesFase(String DesFase) {
+        this.DesFase = DesFase;
+    }
+
+    public String getVigencia() {
+        return Vigencia;
+    }
+
+    public void setVigencia(String Vigencia) {
+        this.Vigencia = Vigencia;
+    }
+
+    @Override
+    public String toString() {
+        return "Fase{" + "CodFase=" + CodFase + ", DesFase=" + DesFase + ", Vigencia=" + Vigencia + '}';
+    }
     
 }
