52a1bcbe8fe8e2b996c313c9cfa9fa37d6f1a5ea
luis.chavez <luchor13@gmail.com>
2017-10-18 09:15:04 -0500

    Creación de la intefaz de constantes BDConstants, donde se almacenan el nombre de usuarios de la bd y la constraña. Modificaciones en la clase PytoDocsDAO

---
+++ src/main/java/edu/unmsm/sistemas/sgpy/constants/BDConstants.java
@@ -0,0 +1,8 @@
+package edu.unmsm.sistemas.sgpy.constants;
+
+public interface BDConstants {
+
+    public final static String SID = "basedatos1";
+    public final static String USER = "sgpy";
+    public final static String PASSWORD = "Prueba$1";
+}
--- src/main/java/edu/unmsm/sistemas/sgpy/repository/imple/PytoDocsDAO.java
+++ src/main/java/edu/unmsm/sistemas/sgpy/repository/imple/PytoDocsDAO.java
@@ -1,64 +1,63 @@
 package edu.unmsm.sistemas.sgpy.repository.imple;
 
-import java.sql.Connection;
-import java.sql.Date;
-import java.sql.PreparedStatement;
-
-import oracle.jdbc.*;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.List;
-
+import edu.unmsm.sistemas.sgpy.constants.BDConstants;
 import edu.unmsm.sistemas.sgpy.entities.PytoDocs;
 import edu.unmsm.sistemas.sgpy.entities.PytoDocs_View;
 import edu.unmsm.sistemas.sgpy.repository.DAOConnection;
 import edu.unmsm.sistemas.sgpy.repository.ModeloIDAO;
-import java.sql.CallableStatement;
-import javax.swing.JOptionPane;
 
-public class PytoDocsDAO implements ModeloIDAO<PytoDocs,PytoDocs_View> {
-    public static final PytoDocsDAO PYTODOCSDAO;
-    
-    static {
-        PYTODOCSDAO = new PytoDocsDAO();
-    }
-    
-    DAOConnection miDao = DAOConnection.getInstance();
-    private String nomb_bd = "basedatos1";
-    private String user = "sgpy";
-    private String pass = "Prueba$1";
-    
-    public static PytoDocsDAO getInstance(){
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.CallableStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import oracle.jdbc.*;
+
+import java.text.SimpleDateFormat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PytoDocsDAO implements ModeloIDAO<PytoDocs, PytoDocs_View> {
+
+    // Crea una instancia de la clase al momento de lanzar la apliacion
+    private static final PytoDocsDAO PYTODOCSDAO = new PytoDocsDAO();
+    // Recoge el objeto DAOConnection
+    private final DAOConnection miDao = DAOConnection.getInstance();
+
+    public static PytoDocsDAO getInstance() {
         return PYTODOCSDAO;
     }
-    
+
     @Override
     public List<PytoDocs_View> listar() {
-        
+
         // ArrayList donde se guardan los documentos
         ArrayList<PytoDocs_View> misProyectos = new ArrayList<>();
 
         try {
-            // Chapa la conexion
-            Connection conn = miDao.getConexion(nomb_bd, user, pass);
+            // Obtener la conexion
+            Connection conn = miDao.getConexion(BDConstants.SID, BDConstants.USER, BDConstants.PASSWORD);
 
+            // Se llama al procedimiento almacenado SP_LISTAR_PYTODOCS
             try (CallableStatement consulta = conn.prepareCall("{ CALL SP_LISTAR_PYTODOCS (?) }")) {
+                // Se pasa por parametro el cursor
                 consulta.registerOutParameter(1, OracleTypes.CURSOR);
+                // Se ejecuta la consulta
                 consulta.execute();
-                
-                try (ResultSet resultado = ((OracleCallableStatement)consulta).getCursor(1)) {
-                    
+
+                try (ResultSet resultado = ((OracleCallableStatement) consulta).getCursor(1)) {
+
                     PytoDocs_View temp;
-                    
                     String verDoc, vigente, desFase, desNivel, desTDoc, desEntreg;
                     int codPyto, corrdocs;
                     Date fecIni, fecFin;
                     double costoEst;
-                    
+
                     while (resultado.next()) {
-                      
+
                         codPyto = resultado.getInt("CodPyto");
                         corrdocs = resultado.getInt("Corrdocs");
                         fecIni = resultado.getDate("FecIni");
@@ -70,10 +69,10 @@ public class PytoDocsDAO implements ModeloIDAO<PytoDocs,PytoDocs_View> {
                         desNivel = resultado.getString("DesNivel");
                         desTDoc = resultado.getString("DesTDoc");
                         desEntreg = resultado.getString("DesEntreg");
-                        
+
                         temp = new PytoDocs_View(codPyto, corrdocs, fecIni, fecFin, costoEst,
                                 verDoc, vigente, desFase, desNivel, desTDoc, desEntreg);
-                        
+
                         misProyectos.add(temp);
                     }
                 }
@@ -88,17 +87,14 @@ public class PytoDocsDAO implements ModeloIDAO<PytoDocs,PytoDocs_View> {
 
     @Override
     public String insertar(PytoDocs nuevo) {
-        // TODO Auto-generated method stub
+
         String msj = "Se insertaron los datos correctamente.";
-        Connection conn = miDao.getConexion(nomb_bd, user, pass);
+        Connection conn = miDao.getConexion(BDConstants.SID, BDConstants.USER, BDConstants.PASSWORD);
         SimpleDateFormat format_fecha = new SimpleDateFormat("dd/MM/yy");
-        String sql = "INSERT INTO PYTODOCS VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
 
         try {
             conn.setAutoCommit(false);
-
-            try ( //PreparedStatement consulta = conn.prepareStatement("INSERT INTO PYTODOCS VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
-                CallableStatement consulta = conn.prepareCall("{ CALL SP_INSERTAR_PYDOCS (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?) }")) {
+            try (CallableStatement consulta = conn.prepareCall("{ CALL SP_INSERTAR_PYDOCS (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?) }")) {
                 consulta.setInt(1, nuevo.getCodPyto());
                 consulta.setInt(2, nuevo.getCodFase());
                 consulta.setInt(3, nuevo.getCodNivel());
@@ -115,10 +111,35 @@ public class PytoDocsDAO implements ModeloIDAO<PytoDocs,PytoDocs_View> {
                 consulta.setInt(14, nuevo.getCodEsp());
                 consulta.setInt(15, nuevo.getCodResp());
                 consulta.setString(16, nuevo.getVigente());
-                
+
                 msj = (consulta.executeUpdate() == 0) ? "No se pudo ejecutar la inserción" : "Correcto";
             }
 
+            conn.commit();
+            miDao.close();
+
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+
+        return msj;
+    }
+
+    @Override
+    public String actualizar(PytoDocs modificacion) {
+        String msj = "Se actualizaron los datos correctamente.";
+        Connection conn = miDao.getConexion(BDConstants.SID, BDConstants.USER, BDConstants.PASSWORD);
+        String sql = "UPDATE proyecto SET Vigente=?, CostoEst=? WHERE cod_tesis = '"
+                + modificacion.getCodDoc() + "'";
+
+        try {
+            conn.setAutoCommit(false);
+            try (PreparedStatement consulta = conn.prepareStatement(sql)) {
+                consulta.setString(1, modificacion.getVigente());
+                consulta.setDouble(2, modificacion.getCostoEst());
+
+                msj = (consulta.executeUpdate() == 0) ? "No se pudo ejecutar la actualizaron de datos" : "Correcto";
+            }
             conn.commit();
             conn.close();
 
@@ -130,41 +151,8 @@ public class PytoDocsDAO implements ModeloIDAO<PytoDocs,PytoDocs_View> {
     }
 
     @Override
-    public String actualizar(PytoDocs modificacion) {
-        // TODO Auto-generated method stub
-        String msj = "Se actualizaron los datos correctamente.";
-        Connection conn = miDao.getConexion(nomb_bd, user, pass);
-        
-        String mensaje;
-
-        String sql = "UPDATE proyecto SET Vigente=?, CostoEst=? WHERE cod_tesis = '"
-                + modificacion.getCodDoc() + "'";
-        
-        actualizar(modificacion);
-	
-	try {
-            conn.setAutoCommit(false);
-            PreparedStatement consulta = conn.prepareStatement(sql);
-            consulta.setString(1, modificacion.getVigente());
-            consulta.setDouble(2, modificacion.getCostoEst());
-           
-            msj = (consulta.executeUpdate() == 0) ? "No se pudo ejecutar la actualizaron de datos" : "Correcto";
-
-            consulta.close();
-            conn.commit();
-            conn.close();
-
-        } catch (SQLException ex) {
-        	System.out.println(ex);
-        }
-         
-        return msj;
-    }
-
-    @Override
     public String eliminar(int codigo) {
         // TODO Auto-generated method stub
         return null;
     }
-
 }
